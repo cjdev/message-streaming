68695293ad1715ea472fca3165aeacdd982d9a65
Anthony Coccia <acoccia@cj.com>
2017-02-15 10:56:54 -0800

    issue CJPM-4902: adding more stuff to make it usable from java

--- src/main/scala/com/cj/messagestreaming/Java.scala
+++ src/main/scala/com/cj/messagestreaming/Java.scala
@@ -1,4 +1,6 @@
 package com.cj.messagestreaming
+import com.cj.messagestreaming.kinesis.Kinesis.KinesisProducerConfig
+
 import scala.concurrent.Future
 
 object Java {
@@ -10,5 +12,11 @@ object Java {
   case class SubscriptionJ(subscription:Subscription) extends Subscription {
     override def mapWithCheckpointing(f: (Array[Byte]) => Unit): Unit = subscription.mapWithCheckpointing(f)
     override def stream: Stream[CheckpointableRecord] = subscription.stream
+  }
+
+  object KinesisProducerConfigJ {
+    def apply(accessKeyId: String, secretKey: String, region: String, streamName: String): KinesisProducerConfig = {
+      KinesisProducerConfig(Some(accessKeyId), Some(secretKey), Some(region), streamName)
+    }
   }
 }
