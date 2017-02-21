4be87eedce6ae749018ae182aeb51fc560239ae8
Anthony Coccia <acoccia@cj.com>
2017-02-20 16:00:00 -0800

    issue CJPM-4902: making a PublishResult that Java can use

--- src/main/scala/com/cj/messagestreaming/Java.scala
+++ src/main/scala/com/cj/messagestreaming/Java.scala
@@ -2,15 +2,24 @@ package com.cj.messagestreaming
 
 
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
+import com.amazonaws.services.kinesis.producer.Attempt
 import com.cj.messagestreaming.kinesis.Kinesis
-import com.cj.messagestreaming.kinesis.Kinesis.{KinesisConsumerConfig, KinesisProducerConfig}
-import com.google.common.util.concurrent.ListenableFuture
-
+import com.cj.messagestreaming.kinesis.Kinesis.{KinesisConsumerConfig, KinesisProducerConfig, KinesisPublishResult}
+import com.google.common.util.concurrent.{Futures, ListenableFuture}
+import com.cj.messagestreaming.kinesis.Kinesis.guavify
 
 object Java {
+
+  case class PublishResultJ(pr: PublishResult) extends PublishResult {
+    override def getAttempts: List[Attempt] = pr.getAttempts
+    override def getSequenceNumber: String = pr.getSequenceNumber
+    override def getShardId: String = pr.getShardId
+    override def isSuccessful: Boolean = pr.isSuccessful
+  }
+
   case class PublicationJ(configJ: KinesisProducerConfigJ) {
     val publication : Publication = Kinesis.makePublication(configJ.config)
-    def publish(bytes: Array[Byte]): ListenableFuture[PublishResult] = publication(bytes)
+    def publish(bytes: Array[Byte]): ListenableFuture[PublishResultJ] = Futures.transform(publication(bytes), PublishResultJ)
   }
 
   case class SubscriptionJ(configJ: KinesisConsumerConfigJ)  {
